6e2fdacc5a5c36da81286f0484fd81370ffdee45
developerkimtakgu <developerkimtakgu@gmail.com>
2023-04-18 17:06:46 +0900

    수정사항 - 코드이그나이터 컨트롤러 테스트

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['web/b_in'] = 'Web/b';
+$route['web/c_in'] = 'Web/c';
---
+++ application/controllers/Web.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class Web extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        echo "aa()";
+    }
+
+    public function b()
+    {
+        echo "b()";
+    }
+
+    public function c()
+    {
+        echo "c()";
+    }
+}
